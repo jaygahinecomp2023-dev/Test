8f5801c1a250a89fa9045ca41762576736924298
jaygahinecomp2023-dev <jay.gahine.comp.2023@vpkbiet.org>
2026-02-15 10:23:41 +0530

    Creating a new file
    this is the file  that we will be pulling using git commands

---
+++ jay.py
@@ -0,0 +1,29 @@
+# This is a comment
+print("Hello, world!")
+
+# Variables
+name = "Alex"
+age = 15
+
+# Printing with variables
+print("Name:", name)
+print("Age:", age)
+
+# Simple math
+a = 10
+b = 5
+print("Sum:", a + b)
+
+# Input from user
+user_name = input("Enter your name: ")
+print("Hello,", user_name)
+
+# If condition
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a teenager")
+
+# Loop
+for i in range(5):
+    print("Number:", i)
